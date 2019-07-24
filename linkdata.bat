069d01f381eeb12633a659335e262b9f815f56a2
raugustyn <Marika56gi>
2019-07-24 12:46:55 +0200

    Backup

---
+++ linkdata.bat
@@ -0,0 +1,3 @@
+cd public
+mklink /D Refrences "C:\Users\Radek Augustyn\Desktop\Gen II\04_KnowledgeBase\Data\References"
+mklink /D data "C:\Users\Radek Augustyn\Desktop\Gen II\04_KnowledgeBase\Data\Droydosvet"
