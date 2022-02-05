b55c106b1a85c54f83749b232915b62574a0d8dc
Radek Augustýn <raugustyn@post.cz>
2022-02-05 09:38:15 +0100

    Working...

--- src/components/timestamp.js
+++ src/components/timestamp.js
@@ -1,23 +1,24 @@
-export function valueToMsg(value, label, tail, allowEmpty) {
-    console.log(value, label, tail, allowEmpty, value + label + tail)
-    if (value > 0) {
-        console.log(value)
-        return (value == 1 ? value + label : value + label + tail)
+export function valueToMsg(value, label, pluralTail) {
+    switch (value) {
+        case 0:
+            return ''
+        case 1:
+            return value + label
+        default:
+            return value + label + pluralTail
     }
-    return (allowEmpty ? '' : 'right now')
 }
 
 export const composeTimestampLabel = (timestamp) => {
     if (timestamp) {
         const daysDiff = (Date.now() - timestamp)/(1000*3600*24)
-
-        if (Math.floor(daysDiff*24*60)) {
+        if (Math.floor(daysDiff*24*60) == 0) {
             return 'right now'
         }
         if (daysDiff < 1.0) {
             const hours = Math.floor(daysDiff*24)
             const minutes = Math.floor((daysDiff*24-hours)*60)
-            return [valueToMsg(hours, ' hour', 's', true), valueToMsg(minutes, ' minut', 'es', false), ' ago'].filter(value => value !== '').join(' ')
+            return [valueToMsg(hours, ' hour', 's', true), valueToMsg(minutes, ' minute', 's', false), ' ago'].filter(value => value !== '').join(' ')
         }
 
         if (daysDiff > 30) {
